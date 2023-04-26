62d06cd6353dac7e703d76c08f66052670ae9d71
JANGALA SREE RAM KRISHNA <58400610+sreeramkrishna22@users.noreply.github.com>
2023-04-26 21:30:58 +0530

    Add files via upload

---
+++ vacationapp/src/test/java/com/employee/vacationapp/VacationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.vacationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
